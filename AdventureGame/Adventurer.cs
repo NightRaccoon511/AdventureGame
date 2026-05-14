db29c6a0a558058e8b20ac8e5f70464deb731244
NightRaccoon511 <74578591+NightRaccoon511@users.noreply.github.com>
2026-05-14 16:19:57 -0400

    Refactor Adventurer class for improved readability
    Refactored Adventurer class to use expression-bodied members and simplified constructor.

--- AdventureGame/Adventurer.cs
+++ AdventureGame/Adventurer.cs
@@ -1,38 +1,11 @@
 namespace AdventureGame;
 
-public class Adventurer
-{
-	private bool hasLamp;
-	private bool hasKey;
+public class Adventurer {
+    private bool hasKey = false;
+    private bool hasLamp = false;
 
-	public Adventurer()
-	{
-		SetLamp(false);
-		SetKey(false);
-	}
-
-	public bool HasLamp()
-	{
-		return hasLamp;
-	}
-
-	public bool HasKey()
-	{
-		return hasKey;
-	}
-
-	public void SetLamp(bool b)
-	{
-		hasLamp = b;
-	}
-
-	public void SetKey(bool b)
-	{
-		hasKey = b;
-	}
-
-	public override string ToString()
-	{
-		return $"Adventurer[hasLamp={hasLamp}, hasKey={hasKey}]";
-	}
+    public bool HasKey() => hasKey;
+    public void SetKey(bool b) => hasKey = b;
+    public bool HasLamp() => hasLamp;
+    public void SetLamp(bool b) => hasLamp = b;
 }
